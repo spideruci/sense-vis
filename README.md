9da0be8409dcaba300f9d37c95c8b160c3e5516e
VijayKrishna Palepu <vijay.0288@gmail.com>
2015-05-14 12:30:47 -0700

    File contents listings on clicking parts of tree-map visualization.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SeeSoftOnD3
+
+
--- js/d3-seesoft.js
+++ js/d3-seesoft.js
@@ -17,7 +17,7 @@ d3.select("body").append("div")
 .style("top", margin.top + "px");
 
 
-function update(root){
+function update(root) {
   //bind new data
   var div = d3.select("body")
         .select("div")
@@ -28,27 +28,84 @@ function update(root){
   
   div.enter().append("div")
             .attr("class", "node")
+            .on('click', function(d) {
+              display_file(d, function(str) {
+                return atob(str).replace(/\n/g, "<br/>");
+              });
+            })
             .transition().duration(750)
             .call(position)
-            .style("background", function(d) { return d.children ? color(d.name) : null; })
-            .text(function(d) { return d.children ? null : d.name; });
+            .style("background", function(d) { 
+              return d.children ? color(d.name) : null; 
+            })
+            .text(function(d) { 
+              return d.children ? null : d.name; 
+            });
+
+  d3.select('body').append('pre').attr('id', 'code')
+
   
   
-  div.attr("class", "node")
-    .transition().duration(750)
-    .call(position)
-      .style("background", function(d) { return d.children ? color(d.name) : null; })
-      .text(function(d) { return d.children ? null : d.name; })
-      .transition().duration(750);
+  // div.attr("class", "node")
+  //   .transition().duration(750)
+  //   .call(position)
+  //   .style("background", function(d) { 
+  //     return d.children ? color(d.name) : null; 
+  //   })
+  //   .text(function(d) { 
+  //     return d.children ? null : d.name; 
+  //   })
+  //   .transition().duration(750);
 }
 
-d3.selectAll("#updateButton").on("click", function() {  
+function display_file(file, decoder) {
+  var codespace = d3.select('#code');
+  codespace.html('');
+  d3.json(file.url, function(err, data) {
+    if(err) {
+      console.log(err);
+      return;
+    }
+    var coded_source = data.content;
+    var source = decoder(coded_source);
+    console.log(file.name);
+    var source_arr = [];
+    source_arr.push(source);
+    codespace.data(source_arr)
+      .html(function(d) { return  d; });
+  });
+}
 
-  var url = 'https://api.github.com/repos/'+d3.select("#user").property('value')+'/'+d3.select("#repo").property('value');
-  d3.json(url+"/branches/master", function(error, data) {
-    url = url + "/git/trees/"+data.commit.sha+"?recursive=1";
-    d3.json(url, function(error, data) {update(parseData(data.tree));});
+d3.selectAll("#updateButton").on("click", function() {
+  var user = d3.select("#user").property('value');
+  var repo = d3.select("#repo").property('value')
+  var url = 'https://api.github.com/repos/'+user+'/'+repo;
 
+  d3.json(url, function(error, repo) {
+    if(error) {
+      console.log(error);
+      return;
+    }
+
+    url = url+"/branches/" + repo.default_branch;
+
+    d3.json(url, function(error, data) {
+      if(error) {
+        console.log(error);
+        return;
+      }
+
+      console.log(data);
+      url = url + "/git/trees/"+data.commit.sha+"?recursive=1";
+      d3.json(url, function(error, data) {
+        if(error) {
+          console.log(error);
+          return;
+        }
+        var root = parseData(data.tree);
+        update(root);
+      });
+    });
   });
 });
 
@@ -57,24 +114,25 @@ function position() {
         .style("top", function(d) { return d.y + "px"; })
         .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
         .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
-  }
+}
+
 function parseData(data) {
   var root = {};
   root.children = [];
   for(var i =0; i < data.length;i++){
     var current = root.children;
-    var steps = data[i].path.split('/'); 
-    for(var j = 0 ; j < steps.length;j++){
+    var steps = data[i].path.split('/');
+    var steps_len = steps.length;
+    for(var j = 0 ; j < steps_len;j++) {
       var step = steps[j];
-      if(j == steps.length-1) {
+      if(j == steps_len - 1) {
         current.push({name:step, size:data[i].size, url:data[i].url});
         break;
       }
       current = current.select(function(v){return v.name == step;});
       current.children =  current.children || [];
       current = current.children;
-      }
-      
+    }
   }
   return root;
 }
