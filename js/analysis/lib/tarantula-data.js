951f5ec60ad696653dd520f568e62e4cbeb0d2ca
VijayKrishna Palepu <vijay.0288@gmail.com>
2015-05-28 21:54:51 -0700

    Tarantula-data API

---
+++ js/analysis/lib/tarantula-data.js
@@ -0,0 +1,35 @@
+// API Docs
+// getFileSusupiciousNess(absoluteSourceFileName)double
+// getLineSusupiciousNess(absoluteSourceFileName, int)double
+// getLineSusupiciousNess(absoluteSourceFileName)double[]
+// getFirstLine(absoluteSourceFileName)int
+// getLastLine(absoluteSourceFileName)int
+
+function TarantulaData() {
+
+}
+
+TarantulaData.prototype.getFileSusupiciousness = function(absoluteSourceFileName) {
+  var fileSusupiciousness = -1.0;
+  return fileSusupiciousness;
+};
+
+TarantulaData.prototype.getLineSusupiciousness = function(absoluteSourceFileName, lineNumber) {
+  var lineSusupiciousness = -1.0;
+  return lineSusupiciousness;
+};
+
+TarantulaData.prototype.getSusupiciousness = function(absoluteSourceFileName) {
+  var lineSusupiciousness = [-1.0];
+  return lineSusupiciousness;
+};
+
+TarantulaData.prototype.getFirstLine = function(absoluteSourceFileName) {
+  var firstLine = 0;
+  return firstLine;
+};
+
+TarantulaData.prototype.getLastLine = function(absoluteSourceFileName) {
+  var lastLine = 0;
+  return lastLine;
+};
--- js/tarantula.js
+++
@@ -1,6 +0,0 @@
-// API Docs
-// getFileSusupiciousNess(absoluteSourceFileName)double
-// getLineSusupiciousNess(absoluteSourceFileName, int)double
-// getLineSusupiciousNess(absoluteSourceFileName)double[]
-// getFirstLine(absoluteSourceFileName)int
-// getLastLine(absoluteSourceFileName)int
