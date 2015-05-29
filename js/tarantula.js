50924e1a3c609dbd6c293b052c88e4af1892a323
VijayKrishna Palepu <vijay.0288@gmail.com>
2015-05-28 17:21:17 -0700

    tarantula.js for interfacing with the suspiciousness values.

---
+++ js/tarantula.js
@@ -0,0 +1,6 @@
+// API Docs
+// getFileSusupiciousNess(absoluteSourceFileName)double
+// getLineSusupiciousNess(absoluteSourceFileName, int)double
+// getLineSusupiciousNess(absoluteSourceFileName)double[]
+// getFirstLine(absoluteSourceFileName)int
+// getLastLine(absoluteSourceFileName)int
